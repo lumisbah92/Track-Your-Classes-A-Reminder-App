33cdf3a59368537fa90d3a4c0a77c26c65f28b9d
Misbah Uddin Tareq <lumisbah92@gmail.com>
2021-10-20 02:37:10 +0600

    temporary gridView Design

--- app/src/main/java/com/example/firebaselearning/ClassActivity.java
+++ app/src/main/java/com/example/firebaselearning/ClassActivity.java
@@ -15,14 +15,14 @@ import androidx.fragment.app.FragmentTransaction;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationView;
 
-public class ClassActivity extends AppCompatActivity{
+public class ClassActivity extends AppCompatActivity {
 
-    private TextView subject,section,ownerName;
-    private BottomNavigationView navigationView;
+    private TextView subject, section, ownerName;
     private String OwnerID = "OwnerID not set";
-    private String OwnerName ="OwnerName not set";
-    private String Subject ="Subject not set";
-    private String Section ="Section not set";
+    private String OwnerName = "OwnerName not set";
+    private String Subject = "Subject not set";
+    private String Section = "Section not set";
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,62 +35,15 @@ public class ClassActivity extends AppCompatActivity{
         ownerName = findViewById(R.id.ownerCardID);
 
         Bundle extras = getIntent().getExtras();
-        if(extras != null)
-        {
+        if (extras != null) {
             Subject = extras.getString("ClassName");
             Section = extras.getString("Section");
             OwnerName = extras.getString("OwnerName");
             OwnerID = extras.getString("OwnerID");
         }
-        OwnerID+=Subject;
+        OwnerID += Subject;
         subject.setText(Subject);
         section.setText(Section);
         ownerName.setText(OwnerName);
-
-        navigationView = findViewById(R.id.navBarID);
-        navigationView.setOnNavigationItemSelectedListener(navListener);
     }
-
-    private BottomNavigationView.OnNavigationItemSelectedListener navListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-                switch (menuItem.getItemId())
-                {
-                    case R.id.ClassInfoID:
-                        ClassInfoFragment classInfoFragment = new ClassInfoFragment();
-                        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
-                        Bundle data = new Bundle();
-                        data.putString("OwnerID", OwnerID);
-                        classInfoFragment.setArguments(data);
-                        fragmentTransaction.replace(R.id.fragmentContainerID, classInfoFragment).commit();
-                        break;
-                    case R.id.ExamInfoID:
-                        ExamInfoFragment examInfoFragment = new ExamInfoFragment();
-                        FragmentTransaction fragmentTransaction1 = getSupportFragmentManager().beginTransaction();
-                        Bundle data1 = new Bundle();
-                        data1.putString("OwnerID", OwnerID);
-                        examInfoFragment.setArguments(data1);
-                        fragmentTransaction1.replace(R.id.fragmentContainerID, examInfoFragment).commit();
-                        break;
-                    case R.id.AssignmentInfoID:
-                        AssignmentInfoFragment assignmentInfoFragment = new AssignmentInfoFragment();
-                        FragmentTransaction fragmentTransaction2 = getSupportFragmentManager().beginTransaction();
-                        Bundle data2 = new Bundle();
-                        data2.putString("OwnerID", OwnerID);
-                        assignmentInfoFragment.setArguments(data2);
-                        fragmentTransaction2.replace(R.id.fragmentContainerID, assignmentInfoFragment).commit();
-                        break;
-                    case R.id.PresentationInfoID:
-                        PresentationInfoFragment presentationInfoFragment = new PresentationInfoFragment();
-                        FragmentTransaction fragmentTransaction3 = getSupportFragmentManager().beginTransaction();
-                        Bundle data3 = new Bundle();
-                        data3.putString("OwnerID", OwnerID);
-                        presentationInfoFragment.setArguments(data3);
-                        fragmentTransaction3.replace(R.id.fragmentContainerID, presentationInfoFragment).commit();
-                        break;
-                }
-
-                return true;
-        }
-    };
 }
